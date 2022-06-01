d0d22c0b95a40271da5731e7f24b21676aa4bebf
ceemos <olaide.suleiman96@gmail.com>
2022-06-01 12:56:12 +0100

    make changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const displayDate = () => {
       const now = DateTime.now();
       const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
       const formatTime = (time) => (time < 10 ? `0${time}` : time);
-      const dateText = `${months[now.month - 1]} ${now.day}th ${now.year}, ${formatTime(now.hour)}:${formatTime(now.minute)}:${formatTime(now.second)}`;
+      const dateText = `${months[now.month - 1]} ${now.day}, ${now.year}, ${formatTime(now.hour)}:${formatTime(now.minute)}:${formatTime(now.second)}`;
       const awesomeDate = document.querySelector('#awesome-date');
       awesomeDate.textContent = dateText;
     }, 1000);
